19b9f1f6bebb639916269dfc54f156876977ec21
friquelmel9 <friquelmel.137@gmail.com>
2026-01-13 17:35:37 -0300

    feat: vim.opt in options.lua

---
+++ lua/config/options.lua
@@ -0,0 +1,7 @@
+-- Vim options
+
+-- Editor
+
+vim.opt.number = true -- Print the line number
+vim.opt.relativenumber = false -- Print the numbers relative to the current line
+vim.opt.autoindent = true -- Copy indent from current line when starting a newline

71e45684e63f6732537b3618205af3e709bf11ed
friquelmel9 <friquelmel.137@gmail.com>
2026-01-15 18:48:03 -0300

    feat(plugins): autopairs plugin added

---
+++ lua/plugins/autopairs.lua
@@ -0,0 +1,11 @@
+-- nvim auto-pairs.
+
+return {
+    "windwp/nvim-autopairs",
+    event = "InsertEnter",
+    config = function()
+        require("nvim-autopairs").setup({
+            disable_filetype = { "TelescopePrompt", "vim" },
+        })
+    end,
+}

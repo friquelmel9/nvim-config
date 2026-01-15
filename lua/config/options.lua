f921b33e118da697c65596ec5d2de3dfbe57b648
friquelmel9 <friquelmel.137@gmail.com>
2026-01-15 18:45:45 -0300

    feat(options.lua): Tab & whitespaces options

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -4,4 +4,17 @@
 
 vim.opt.number = true -- Print the line number
 vim.opt.relativenumber = false -- Print the numbers relative to the current line
+
+-- Write options
 vim.opt.autoindent = true -- Copy indent from current line when starting a newline
+vim.opt.tabstop = 4 -- Tab considered as spaces
+vim.opt.smartindent = true -- Auto indent
+vim.opt.title = false -- When on, the title of the window will be set to the value of 'titlestring'
+
+-- Whitespace
+vim.opt.list = true -- See whitespaces
+vim.opt.listchars = 'tab:» ,lead:•,trail:•' -- Change whitespaces chars
+
+-- Other
+vim.opt.showcmd = true -- Show partial command
+

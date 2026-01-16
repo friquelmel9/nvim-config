ad3c991c2e75a87f3ae9d6cfdfd289332fe35598
friquelmel9 <friquelmel.137@gmail.com>
2026-01-16 13:25:24 -0300

    feat(plugins/colorscheme.lua): neomodern theme added

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,4 +1,22 @@
+-- colorscheme.lua: Selects the color scheme
+-- To change the color scheme, quit the comment of the return package
+
+--[[ Monochrome.nvim: Monochrome setting
 return {
     "kdheepak/monochrome.nvim",
     priority = 1000,
+}
+]]
+
+-- Neomodern.nvim: Purple themne
+return {
+  "cdmill/neomodern.nvim",
+  lazy = false,
+  priority = 1000,
+  config = function()
+    require("neomodern").setup({
+      -- optional configuration here
+    })
+    require("neomodern").load()
+  end,
 }

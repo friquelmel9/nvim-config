10917634ab0262f24b1f9021e4f205e7b1dd7d5f
friquelmel9 <friquelmel.137@gmail.com>
2026-01-19 17:24:48 -0300

    feat(plugins): mason-lspconfig, mason.nvim & lspconfig.nvim

--- lua/config/maps.lua
+++ lua/config/maps.lua
@@ -46,3 +46,9 @@ vim.keymap.set('n','<C-j>','<C-w>j') -- Move to a window below
 -- For movement as visual using j and k
 vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
 vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
+
+-- Vim diagnostic
+vim.keymap.set('n','<leader>e', vim.diagnostic.open_float, {
+	desc = 'show error floating'
+})
+
---
+++ lua/plugins/mason.lua
@@ -0,0 +1,10 @@
+-- Mason.lua: For plugins. It includes various plugins for this
+
+return{
+    "mason-org/mason-lspconfig.nvim",
+    opts = {},
+    dependencies = {
+        { "mason-org/mason.nvim", opts = {} },
+        "neovim/nvim-lspconfig",
+    },
+}

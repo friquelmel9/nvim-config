d0c4f6dd5122dec0794b53fe9c9dc3ea9456b221
friquelmel9 <friquelmel.137@gmail.com>
2026-02-02 20:21:16 -0300

    feat(maps.lua): Function to simplify keymaps

--- lua/config/maps.lua
+++ lua/config/maps.lua
@@ -1,11 +1,23 @@
--- Keymapings
+--[[
+-- A function to simplify keymaps
+--
+-- @param mode: Mode that the keymap is used
+-- @param lhs: Key combination to use the mapping
+-- @param rhs: True combination used in nvim
+-- @param opts: Options for the keymap
+--]]
+local function keymap(mode, lhs, rhs, opts)
+	local options = { silent = true }
+	if opts then
+		options = vim.tbl_deep_extend("force",options,opts)
+	end
+	vim.keymap.set(mode,lhs,rhs,options)
+end
 
--- Neotree
-vim.keymap.set('n','<leader>e','<CMD>Neotree toggle<CR>', {silent = true})
-vim.keymap.set('n','<leader>r','<CMD>Neotree focus<CR>', {silent = true})
-
--- From the Neotree repository
-vim.keymap.set('n', '-', function()
+-- Keymaps Neotree.
+keymap('n','<leader>e','<CMD>Neotree toggle<CR>') -- I don't know what this does.
+keymap('n','<leader>r','<CMD>Neotree focus<CR>') -- Focus in the Neotree window
+vim.keymap.set('n', '-', function() -- From the Neotree repository
     local reveal_file = vim.fn.expand('%:p')
     if (reveal_file == '') then
       reveal_file = vim.fn.getcwd()
@@ -28,24 +40,24 @@ vim.keymap.set('n', '-', function()
     { desc = "Open neo-tree at current file or working directory" }
 )
 
--- From the Neogit repository
-vim.keymap.set('n','<leader>git','<CMD>Neogit<CR>', {
-	desc = "Open Neogit UI"
-})
+-- Keymaps Neogit
+keymap('n','<leader>git','<CMD>Neogit<CR>', {desc = "Open Neogit UI" }) -- From Neogit repository
 
--- New windows
-vim.keymap.set('n','<leader>o','<CMD>vsplit<CR>') -- Split the window (Vertical)
-vim.keymap.set('n','<leader>p','<CMD>split<CR>') -- Split the window
+-- Window Management
+keymap('n','<leader>o','<CMD>vsplit<CR>', {desc = "Split the window (vertical)"})
+keymap('n','<leader>p','<CMD>split<CR>', {desc = "Split the window (horizontal)"})
+keymap('n',"<C-h>","<C-w>h", {desc = "Move to a window left from current"})
+keymap('n','<C-l>','<C-w>l', {desc = "Move to a window right from current"})
+keymap('n','<C-k>','<C-w>k', {desc = "Move to a window top from current"})
+keymap('n','<C-j>','<C-w>j', {desc = "Move to a window bottom from current"})
 
--- Window Navigation
-vim.keymap.set('n','<C-h>','<C-w>h') -- Move to a window in the left
-vim.keymap.set('n','<C-l>','<C-w>l') -- Move to a window in the right
-vim.keymap.set('n','<C-k>','<C-w>k') -- Move to a window above
-vim.keymap.set('n','<C-j>','<C-w>j') -- Move to a window below
+-- Movement Configurations
+keymap('n', 'k', "v:count == 0 ? 'gk' : 'k'", { desc = "Visual Movement in Normal Mode for k",
+	expr = true, silent = true })
+keymap('n', 'j', "v:count == 0 ? 'gj' : 'j'", { desc = "Visual Movement in Normal Mode for j"
+	expr = true, silent = true})
 
--- For movement as visual using j and k
-vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
-vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
+
 
 -- Vim diagnostic
 vim.keymap.set('n','<leader>e', vim.diagnostic.open_float, {
